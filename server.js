645dd76aa233451f3183c344c555df14725009d7
OGurso <70133220+OGurso@users.noreply.github.com>
2021-05-14 14:08:03 +0200

    lab 2 is finished

--- server.js
+++ server.js
@@ -3,40 +3,15 @@ const mysql = require("mysql");
 const app = express();
 const port = 3000;
 
-//av någon anledning kunde jag inte POST:a utan dessa.
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// connection configurations
+//database-connection configurations
 var dbConn = mysql.createConnection({
   host: "remotemysql.com",
   user: "Tr8gRDvO3X",
   password: "5RAIRKGqXM",
   database: "Tr8gRDvO3X",
-});
-
-//  CRUD med node.js och SQL -------------------
-// When creating a table, you should also create a column with a unique key for each record.
-//This can be done by defining a column as "INT AUTO_INCREMENT PRIMARY KEY" which will insert a unique number for each record. Starting at 1, and increased by one for each record.
-
-// dbConn.connect(function (err) {
-//   if (err) throw err;
-//   console.log("Connected!");
-//   var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
-//   dbConn.query(sql, function (err, result) {
-//     if (err) throw err;
-//     console.log("Table created");
-//   });
-// });
-
-dbConn.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected!");
-  var sql = "CREATE TABLE todos (id INT, todo VARCHAR(100))";
-  dbConn.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("Table created");
-  });
 });
 
 app.get("/todo", (req, res) => {
@@ -45,16 +20,17 @@ app.get("/todo", (req, res) => {
 
 // -----------------------------------------------
 
+//randomgenerator
 const custom_random = (num) => {
-  //randomgenerator
   return parseInt(Math.random() * num);
 };
 
+//the server responds with a
 app.get("/", (req, res) => {
-  //här svarar servern med en hel fil
   res.sendFile(`${__dirname}/public/index.html`);
 });
 
+//each time this endpoint is called a random number is generated between 0-1023
 app.get("/api/random", (req, res) => {
   let rand = {
     number: custom_random(1023),
@@ -68,8 +44,9 @@ app.get(`/api/custom_random/:num`, (req, res) => {
   };
   res.send(rand);
 });
-app.post("/api/vokaler", (req, res) => {
-  let ordet = Array.from(req.body.ord.toUpperCase());
+
+app.post("/api/vokaler/:ord", (req, res) => {
+  let ordet = Array.from(req.params.ord.toUpperCase());
   let vokaler = ["A", "E", "I", "O", "U", "Y", "Å", "Ä", "Ö"];
   let antal = ordet.filter((x) => vokaler.includes(x));
   let response = {
@@ -79,16 +56,59 @@ app.post("/api/vokaler", (req, res) => {
 });
 
 let count = 0; //counter för "/counter" endpoint
-app.get("/counter/increment", (req, res) => {
+app.get("/counter/add", (req, res) => {
   count++;
   res.send(
-    "The counter is incremented each time you visit this endpoint > <a href='/counter/view'>See the count</a>"
+    "The counter is incremented each time you visit this endpoint > <a href='/counter/show'>See the count</a>"
   );
 });
 
-app.get("/counter/view", (req, res) => {
-  res.send(`${count} <a href='/counter/increment'>back to increment</a>`);
+app.get("/counter/show", (req, res) => {
+  res.send(`${count} <a href='/counter/add'>back to increment</a>`);
 });
+
+// SQL/node.js CRUD operations ----------------------->
+//create new todo - everything that's written after create/ becomes the text that's saved to the database
+app.post("/db/create/:text", (req, res) => {
+  let text = req.params.text;
+  dbConn.query(
+    "INSERT INTO `todos` (`todo`) VALUES ('" + text + "');",
+    (err, result, fields) => {
+      res.send(result);
+    }
+  );
+});
+
+//read all todos that's saved in the db
+app.get("/db/readall", (req, res) => {
+  dbConn.query("SELECT * FROM `todos`", (err, result, fields) => {
+    res.send(result);
+  });
+});
+
+//the todo with the given id in :id section is updated to whatever that's given in the :text section
+app.get("/db/update/:id/:text", (req, res) => {
+  let id = req.params.id;
+  let text = req.params.text;
+  dbConn.query(
+    "UPDATE `todos` SET `todo` = '" + text + "' WHERE `todos`.`id` =" + id,
+    (err, result, fields) => {
+      res.send(result);
+    }
+  );
+});
+
+//the row with the id that's typed in the endpoint is deleted from the db
+app.get("/db/delete/:id", (req, res) => {
+  let id = req.params.id;
+  dbConn.query(
+    "DELETE FROM `todos` WHERE `todos`.`id` = " + id,
+    (err, result, fields) => {
+      res.send(result);
+    }
+  );
+});
+// CRUD end-------------------------------
 
 app.listen(port, () => {
   console.log(`Server is online & listening to port ${port}.`);
